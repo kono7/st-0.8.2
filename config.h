f81b407202010d7c15dc3a498b7a4d11764d7450
Martin Wilson <martin@pipegrep.co.uk>
2019-03-10 12:26:20 +0000

    alpha patch applied

--- config.h
+++ config.h
@@ -82,6 +82,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	"#1d2021", /* hard contrast: #1d2021 / soft contrast: #32302f */
@@ -106,6 +109,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+    "black",
 };
 
 
@@ -114,7 +118,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
